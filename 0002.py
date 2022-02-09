ddcf1324bd78b5e8bb87aa4d1210bf1c08d5e6eb
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-02-10 01:13:13 +0530

    .

---
+++ 0002.py
@@ -0,0 +1,15 @@
+import re
+
+
+def fib(a, b):
+    s = 2
+    c = 0
+    while c < 4000000:
+        c = a + b
+        if c % 2 == 0:
+            s += c
+        a = b
+        b = c
+    return s
+
+print(fib(1, 2))
---
+++ 0003.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int check_prime(long long int num)
+{
+    // long long int r = sqrt(num);
+    long long int r = sqrt(num);
+    for (long long int i = 2; i < r; ++i)
+    {
+        if (num % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void max_pfac(long long int num)
+{
+    long long int r = sqrt(num);
+    for (long long int i = r; i > 0; --i)
+    {
+        if (num % i == 0)
+        {
+            if (check_prime(i))
+            {
+                printf("%lld\n", i);
+                break;
+            }
+        }
+    }
+    
+}
+
+int main(void)
+{
+    long long int num = 600851475143;
+    max_pfac(num);
+
+    return 0;
+}
