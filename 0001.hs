a7129bf375630e6b96d0e428572022e403d6bc2b
varun <74466129+nairvarun@users.noreply.github.com>
2023-01-10 15:10:23 +0530

    .

--- 0001.hs
+++ 0001.hs
@@ -1,4 +1,3 @@
 main :: IO()
 main = do
-    let muls = sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0 ]
-    print(muls)
+    print(sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0 ])
